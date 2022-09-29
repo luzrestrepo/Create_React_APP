6e75e9161eba05cd579cc929af1efbbfdbdb8962
Luz  Restrepo <106494261+luzrestrepo@users.noreply.github.com>
2022-09-28 20:32:22 -0500

    Create_React_App

---
+++ create-react-app/intro-react/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class App extends React.Component {
+
+    render() {
+        return ( 
+          <div>
+            <h1>Hello Dojo!</h1>
+            <h2>Things ineed to do;</h2>
+            <ul>
+              <li>Learn React</li>
+              <li>Climb Mt. Everest</li>
+              <li>Run a marathon</li>
+              <li>Feed the dogs</li>
+            </ul>
+          </div>   
+          
+        );
+    }
+
+}
+
+export default App;
